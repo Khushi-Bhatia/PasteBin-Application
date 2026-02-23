31ab0ead992fe17724ed40419125ad7697bb8090
Khushi-Bhatia <khushibhatiaabc@gmail.com>
2026-02-23 20:28:32 +0530

    Fix CORS for production

--- pastebin/src/main/java/com/example/pastebin/controller/PasteController.java
+++ pastebin/src/main/java/com/example/pastebin/controller/PasteController.java
@@ -14,7 +14,7 @@ import org.springframework.web.util.HtmlUtils;
 import java.time.Instant;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 public class PasteController {
 
