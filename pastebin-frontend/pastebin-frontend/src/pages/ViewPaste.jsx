987a002eaaa80ac359c0597f8a9d627622d3d0b6
Khushi-Bhatia <khushibhatiaabc@gmail.com>
2026-02-24 17:06:40 +0530

    added console to debug

--- pastebin-frontend/pastebin-frontend/src/pages/ViewPaste.jsx
+++ pastebin-frontend/pastebin-frontend/src/pages/ViewPaste.jsx
@@ -72,6 +72,64 @@
 // }
 
 
+// import { useParams } from "react-router-dom";
+// import { useEffect, useState } from "react";
+
+// const API = import.meta.env.VITE_API_URL;
+
+// export default function ViewPaste() {
+//   const { id } = useParams();
+
+//   const [content, setContent] = useState("");
+//   const [remainingViews, setRemainingViews] = useState(null);
+//   const [expiresAt, setExpiresAt] = useState(null);
+//   const [error, setError] = useState(false);
+
+//   useEffect(() => {
+//     const fetchPaste = async () => {
+//       try {
+//         console.log("API:", API);
+//         const baseUrl = API.replace(/\/$/, "");
+//         console.log("Base URL:", baseUrl);
+//         console.log("Fetching paste ID:", id);
+
+//         const res = await fetch(`${baseUrl}/api/pastes/${id}`);
+//         console.log("Response status:", res.status);
+
+//         if (!res.ok) {
+//           setError(true);
+//           return;
+//         }
+
+//         const data = await res.json();
+//         console.log("Paste data:", data);
+
+//         setContent(data.content);
+//         setRemainingViews(data.remaining_views);
+//         setExpiresAt(data.expires_at);
+//       } catch (err) {
+//         console.error("Fetch failed:", err);
+//         setError(true);
+//       }
+//     };
+//      console.log("Final URL:", `${baseUrl}/api/pastes/${id}`);
+
+//     fetchPaste();
+//   }, [id]);
+
+//   if (error) return <h2>Paste Not Found</h2>;
+
+//   return (
+//     <div style={{ padding: 40 }}>
+//       <h2>Paste Content</h2>
+//       <pre>{content}</pre>
+//       {remainingViews !== null && <p>Remaining Views: {remainingViews}</p>}
+//       {expiresAt && <p>Expires At: {expiresAt}</p>}
+//     </div>
+//   );
+// }
+
+
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
@@ -88,10 +146,19 @@ export default function ViewPaste() {
   useEffect(() => {
     const fetchPaste = async () => {
       try {
-        const baseUrl = API.replace(/\/$/, "");
-        console.log("Fetching paste ID:", id);
+        if (!API) {
+          console.error("VITE_API_URL is undefined");
+          setError(true);
+          return;
+        }
 
-        const res = await fetch(`${baseUrl}/api/pastes/${id}`);
+        const baseUrl = API.replace(/\/$/, "");
+        const finalUrl = `${baseUrl}/api/pastes/${id}`;
+
+        console.log("API:", API);
+        console.log("Final URL:", finalUrl);
+
+        const res = await fetch(finalUrl);
         console.log("Response status:", res.status);
 
         if (!res.ok) {
